51c128e5b23c3955112a939585d5fc00e2794168
farisqlail <faris.riskilail@gmail.com>
2024-01-10 23:35:47 +0700

    title navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -4,11 +4,11 @@
       <div class="container">
         <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
 
-        <b-navbar-brand>BootstrapVue</b-navbar-brand>
+        <b-navbar-brand>Riset PWA Nuxt Js</b-navbar-brand>
 
         <b-collapse id="nav-text-collapse" is-nav>
           <b-navbar-nav>
-            <b-nav-text>Navbar text</b-nav-text>
+            <!-- <b-nav-text>Navbar text</b-nav-text> -->
           </b-navbar-nav>
         </b-collapse>
       </div>
